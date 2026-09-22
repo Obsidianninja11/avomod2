8d8d8d57a7bb680f2719cab240ba6c037a5bf9e4
Obsidian <108832807+Obsidianninja11@users.noreply.github.com>
2026-09-22 03:33:11 -0800

    Better disable place i think

--- src/main/java/cf/avicia/avomod2/client/eventhandlers/inventoryclickedevents/TriggerInventoryMouseClickedEvents.java
+++ src/main/java/cf/avicia/avomod2/client/eventhandlers/inventoryclickedevents/TriggerInventoryMouseClickedEvents.java
@@ -11,7 +11,9 @@ import java.util.List;
 
 public class TriggerInventoryMouseClickedEvents {
     public static ActionResult mouseClicked(double mouseX, double mouseY, int button, Slot clickedSlot, ScreenHandler screenHandler) {
-        if (ConfigsHandler.getConfigBoolean("disableAll")) return ActionResult.SUCCESS;
+        if (ConfigsHandler.getConfigBoolean("disableAll") ||
+            !ConfigsHandler.getConfigBoolean("tradeMarketMiddleClickSearch")
+        ) return ActionResult.SUCCESS;
 
         List<ActionResult> actionResults = new ArrayList<>();
         actionResults.add(TradeMarketMiddleClickSearch.mouseClicked(mouseX, mouseY, button, clickedSlot, screenHandler));
